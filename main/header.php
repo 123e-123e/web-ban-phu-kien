00be5817a109e9b129c1db5552ccfc48ed399094
khoinguyen162 <kn4267909@gmail.com>
2026-04-16 21:33:08 +0700

    Update header.php

--- main/header.php
+++ main/header.php
@@ -5,6 +5,7 @@
     <input type="text" placeholder="Nhập sản phẩm bạn muốn tìm kiếm...">
     <button type="submit" id="search"><ion-icon name="search"></ion-icon></button>
     <a href="account/login.php">Đăng Nhập</a>
+    <a href="account/register.php">Đăng Ký</a>
     <a href="elements/favorites.php"><ion-icon name="heart"></ion-icon></a>
     <a href="elements/view-cart.php"><ion-icon name="cart"></ion-icon></a>
 </form>
