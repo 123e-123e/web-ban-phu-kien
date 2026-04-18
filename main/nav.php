08e3b2d2ac074c52fb9d3376fff1263bfb821dbd
123e-123e <trangiabai23320088@gmail.com>
2026-04-18 18:37:46 +0700

    Update nav.php

--- main/nav.php
+++ main/nav.php
@@ -9,22 +9,23 @@
                     </a>
                 </li>
                 <ul id="menu-bar">
-                    <li><ion-icon name="laptop-outline"></ion-icon><a href="#"> LAPTOP Nhập khẩu</a></li>
-                    <li><ion-icon name="hardware-chip-outline"></ion-icon><a href="#"> Phụ kiện máy tính</a></li>
-                    <li><ion-icon name="flower-outline"></ion-icon><a href="#"> Linh kiện máy tính</a></li>
-                    <li><ion-icon name="tv-outline"></ion-icon><a href="#"> Máy tính bộ</a></li>
-                    <li><ion-icon name="tablet-portrait-outline"></ion-icon><a href="#"></ion-icon> PC mini</a></li>
-                    <li><ion-icon name="desktop-outline"></ion-icon><a href="#"> Màn hình</a></li>
-                    <li><ion-icon name="apps-outline"></ion-icon><a href="#"> Phần mềm bản quyền</a></li>
-                </ul>
-            </nav>
+                <li><ion-icon name="laptop-outline"></ion-icon><a href="#"> LAPTOP Nhập khẩu</a></li>
+                <li><ion-icon name="hardware-chip-outline"></ion-icon><a href="#"> Phụ kiện máy tính</a></li>
+                <li><ion-icon name="flower-outline"></ion-icon><a href="#"> Linh kiện máy tính</a></li>
+                <li><ion-icon name="tv-outline"></ion-icon><a href="#"> Máy tính bộ</a></li>
+                <li><ion-icon name="tablet-portrait-outline"></ion-icon><a href="#"> PC mini</a></li>
+                <li><ion-icon name="desktop-outline"></ion-icon><a href="#"> Màn hình</a></li>
+                <li><ion-icon name="apps-outline"></ion-icon><a href="#"> Phần mềm bản quyền</a></li>
+            </ul>
         </li>
-        <li><a href="index.php">TRANG CHỦ</a></li>
-        <li><a href="elements/products.php">SẢN PHẨM</a></li>
-        <li><a href="elements/repairs.php">SỮA CHỮA</a></li>
-        <li><a href="elements/warities.php">TRUNG TÂM BẢO HÀNH</a></li>
-        <li><a href="elements/news.php">TIN CÔNG NGHỆ</a></li>
-        <li><a href="elements/vlogs.php">VLOG THỦ THUẬT</a></li>
-        <li><a href="elements/recruitments.php">TUYỂN DỤNG</a></li>
-        <li><a href="elements/contacts.php">LIÊN HỆ</a></li>
+        <li style="display: inline-block; margin-right: 20px;"><a href="index.php" style="text-decoration: none; color: white;">TRANG CHỦ</a></li>
+        <li style="display: inline-block; margin-right: 20px;"><a href="elements/products.php" style="text-decoration: none; color: white;">SẢN PHẨM</a></li>
+        <li style="display: inline-block; margin-right: 20px;"><a href="elements/repairs.php" style="text-decoration: none; color: white;">SỬA CHỮA</a></li>
+        <li style="display: inline-block; margin-right: 20px;"><a href="elements/warities.php" style="text-decoration: none; color: white;">TRUNG TÂM BẢO HÀNH</a></li>
+        <li style="display: inline-block; margin-right: 20px;"><a href="elements/news.php" style="text-decoration: none; color: white;">TIN CÔNG NGHỆ</a></li>
+        <li style="display: inline-block; margin-right: 20px;"><a href="elements/vlogs.php" style="text-decoration: none; color: white;">VLOG THỦ THUẬT</a></li>
+        <li style="display: inline-block; margin-right: 20px;"><a href="elements/recruitments.php" style="text-decoration: none; color: white;">TUYỂN DỤNG</a></li>
+        <li style="display: inline-block;"><a href="elements/contacts.php" style="text-decoration: none; color: white;">LIÊN HỆ</a></li>
+    </ul>
 </nav>
+
