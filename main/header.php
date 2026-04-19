a6010c14a26cec7dbd43f01c31c97ccd27b404fc
khoinguyen162 <kn4267909@gmail.com>
2026-04-19 15:18:46 +0700

    Update header.php

--- main/header.php
+++ main/header.php
@@ -1,6 +1,6 @@
 
 <header>
-<img height="50px" width="200px" src="img/logo/LAPTOPZZ.png" alt="logo-laptopz1" id="logo">
+<img height="50px" width="200px" src="img/logo/logo.svg" alt="logo-laptopz1" id="logo">
 <form action="functions/search-view.php" method="post">
     <input type="text" placeholder="Nhập sản phẩm bạn muốn tìm kiếm...">
     <button type="submit" id="search"><ion-icon name="search"></ion-icon></button>
