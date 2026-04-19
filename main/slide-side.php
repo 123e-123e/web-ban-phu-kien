91e235bd0212a4076a41106641df61ce59c8315d
khoinguyen162 <kn4267909@gmail.com>
2026-04-19 15:12:07 +0700

    Update slide-side.php

--- main/slide-side.php
+++ main/slide-side.php
@@ -5,11 +5,11 @@
     <input type="radio" name="slide" id="s3">
     <input type="radio" name="slide" id="s5">
     <div class="slides">
-        <img src="img/banner/slide1.svg">
-        <img src="img/banner/slide2.svg">
-        <img src="img/banner/slide3.svg">
-        <img src="img/banner/slide4.svg">
-        <img src="img/banner/slide5.svg">
+        <img src="img/banner/bn1.webp">
+        <img src="img/banner/bn2.webp">
+        <img src="img/banner/bn3.webp">
+        <img src="img/banner/bn4.webp">
+        <img src="img/banner/bn5.webp">
     </div>
     <div class="arrow prev" onclick="prevSlide()">❮</div>
     <div class="arrow next" onclick="nextSlide()">❯</div>
