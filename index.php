73857251b86a2afd2a90fa80dc6eba7a18f16579
khoinguyen162 <kn4267909@gmail.com>
2026-04-20 17:11:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trang Chủ</title>
     <link rel="stylesheet" src="style/main.css">
+    <link rel="icon" type="image/svg+xml" sizes="48x48" href="img/favicon/favicon.svg">
     <link rel="stylesheet" href="style/style.css">
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
