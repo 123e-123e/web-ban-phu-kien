052b5be0bf7ba63d22a0bf1ad13a8eacab012127
123e-123e <trangiabai23320088@gmail.com>
2026-04-20 19:57:40 +0700

    Add files via upload

--- index1.php
+++ index1.php
@@ -71,7 +71,6 @@ if ($result && $result->num_rows > 0) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/svg+xml" sizes="48x48" href="img/favicon/favicon.svg">
     <title>Quản Trị LaptopZZ</title>
     <link rel="stylesheet" href="/style/style_index.css?v=<?php echo time(); ?>">
 </head>
